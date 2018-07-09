5aba7aed0a31c65204c4d61cecf631c96fb45fcc
Taylor Morris <taylor.morris@gmail.com>
2018-07-09 16:47:47 -0700

    quick comment and  new working branch

--- app.py
+++ app.py
@@ -3,3 +3,4 @@ from flask import Flask
 app = Flask(__name__)
 db_uri = 'mysql+pymysql://root:homespringmaria@localhost/feedreader'
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+# and no that's not my ~actual~ root password... it's just one for this project :)
